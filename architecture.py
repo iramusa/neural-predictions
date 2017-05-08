38a49092a7cbb27e0a8270b6edfa396c47009049
Marian Andrecki <marian.andrecki@gmail.com>
2017-05-09 00:07:41 +0200

    Split real and fake batch for disc training.

--- architecture.py
+++ architecture.py
@@ -186,23 +186,17 @@ class MultiNetwork(object):
             self.autoencoder_disc.compile(optimizer=Adam(lr=0.0001), loss='binary_crossentropy')
             # raise ValueError('Discriminator must be trainable')
 
-        batch_size = 64
-
-        labels = np.zeros((batch_size,))
-        labels[:int(batch_size/2)] = 1
-
-        # indices = np.random.randint(0, real_images.shape[0], size=int(batch_size/2))
-        # real = real_images[indices, ...]
+        batch_size = 32
 
         # generate fake images
         fake_images = self.autoencoder_gen.predict(real_images)
 
-        train = np.concatenate((real_images, fake_images))
-
         if test:
-            loss = self.autoencoder_disc.test_on_batch(train, labels)
+            loss = self.autoencoder_disc.test_on_batch(real_images, np.ones((batch_size))) + \
+                   self.autoencoder_disc.test_on_batch(fake_images, np.zeros((batch_size)))
         else:
-            loss = self.autoencoder_disc.train_on_batch(train, labels)
+            loss = self.autoencoder_disc.train_on_batch(real_images, np.ones((batch_size))) + \
+                   self.autoencoder_disc.train_on_batch(fake_images, np.zeros((batch_size)))
 
         return loss
 
